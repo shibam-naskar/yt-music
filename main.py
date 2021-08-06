c69c1a89d9a0a694eb50939f6c30f10ece9ae529
shibam-naskar <shibamnaskar2@gmail.com>
2021-08-06 20:41:59 +0530

    error fixed

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def youtubeMusic(n):
 def youtubePlaylist(p):
     playlist=PlaylistsSearch(p,limit=1)
     url = playlist.result()['result'][0]['link']
-    playlist_final = Playlist(url,limit=20)
+    playlist_final = Playlist(url)
     return jsonify(playlist_final.videos)
 
 
